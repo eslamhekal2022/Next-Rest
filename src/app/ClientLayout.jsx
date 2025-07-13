a128df44f7e25a43991be52318c85765c4ec3e85
eslamhekal2022 <eslamhekal2022@gmail.com>
2025-07-13 15:09:53 +0300

    fix name Comp

--- src/app/ClientLayout.jsx
+++ src/app/ClientLayout.jsx
@@ -6,7 +6,7 @@ import { CartProvider } from "@/src/context/CartContext.js";
 import { ProductProvider } from "@/src/context/productContext.js";
 import { UserProvider } from "@/src/context/userContext.js";
 import "./globals.css"
-import Navbar from "../(components)/navbar/page";
+import Navbar from "../(components)/navbar/Navbar";
 export default function ClientLayout({ children }) {
   return (
     <Provider store={store}>
